0a054591fed9967a421f41b217131a63d53b2914
Alex Middeleer <amiddeleer@gmail.com>
2014-04-03 11:15:37 -0400

    now emits fd along with new lines.  Also, need to fix bug where if extension is specified subdirectory files are not watched

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ var tailMod = require('file-tail')
 ,   fs = require ('fs')
 ,   minimatch = require ('minimatch')
 ,   events = require ('events')
+,   path = require ('path')
 ;
 
-module.exports.startTailing = function(fd, errCB, ext){
+module.exports.startTailing = function(fd, errCB, ext, interval){
 	ext = ext || "*";
+	interval = interval || 1000;
 
 	var folderWatcher = new events.EventEmitter()
 	, tailObjs = {}
@@ -13,6 +15,9 @@ module.exports.startTailing = function(fd, errCB, ext){
 	, folderInit
 	, onErr
 	, firstPass = true 
+	, start
+	, restart
+	, pulse
 	;
 
 	onErr = function(err) {
@@ -23,38 +28,35 @@ module.exports.startTailing = function(fd, errCB, ext){
 	// This function starts tailing a file or directory. Recursive.
 	// -----------------------------------------------------
 	tailInit = function(fd) {
-		// console.log('in tailInit, inspecting file...' + fd);
 		fs.stat(fd, function(err, stats) {
 			onErr(err);
 			if (stats.isDirectory()) {
 				folderInit(fd + '/');
 			}else{
 				if (!tailObjs[fd]) {
-					// console.log('in tailInit, starting to tail file...' + fd);
 					var tailObj = tailMod.startTailing(fd);
 					tailObjs[fd]=tailObj;
 					tailObj.on('line', function(line) {
-						folderWatcher.emit('line', line);
+						folderWatcher.emit('line', line, path.normalize(fd));
 					});
+					
 					// -----------------------------------------------------
 					// After the first pass new files will be emitted as
 					// if they had grown from 0 bytes
 					// -----------------------------------------------------
 					if (!firstPass) {
-						// console.log('Found a new file, not in first pass so emitting lines');
 						fs.readFile(fd,function(err, data) {
 							if (err) {
 								console.log(err);
 							} else{
 								var lines = data.toString().trim().split('\n');	
-								for( var i = 0; i < lines.length; i++ ){
-									folderWatcher.emit('line', lines[i]);
+								for ( var i = 0; i < lines.length; i++ ){
+									folderWatcher.emit('line', lines[i], path.normalize(fd));
 								}
 							}
 						});
 					}
 				} else {
-					// console.log("in tailInit already watching " + fd);
 				}
 			}
 		});
@@ -64,13 +66,11 @@ module.exports.startTailing = function(fd, errCB, ext){
 	// This function starts tailing all the things in a directory. Recursive.
 	// -----------------------------------------------------
 	folderInit =  function(fd) {
-		// console.log('in folderInit, tailing folder...' + fd);
-
 		var i
 		, fdsToTail = []
 		;
+
 		fs.readdir(fd, function(err, files) {
-			onErr(err);
 			if (!err) {
 				for (i = 0; i < files.length; i++) {
 					if(minimatch(files[i], ext)){
@@ -78,9 +78,11 @@ module.exports.startTailing = function(fd, errCB, ext){
 					}
 				}
 				
-				for (i = 0; i < fdsToTail.length; i++) {
+				for ( i = 0; i < fdsToTail.length; i++ ) {
 					tailInit(fdsToTail[i]);
 				}
+			}else{
+				onErr(err);
 			}
 		});
 	};
@@ -89,7 +91,7 @@ module.exports.startTailing = function(fd, errCB, ext){
 	// -----------------------------------------------------
 	// Start watching the directory
 	// -----------------------------------------------------
-	var start = function() {
+	start = function() {
 		folderInit(fd);
 	}
 	start();
@@ -97,12 +99,11 @@ module.exports.startTailing = function(fd, errCB, ext){
 	// -----------------------------------------------------
 	// Every so often check for new files
 	// -----------------------------------------------------
-	var restart = function() {
-		// console.log("restarting");
+	restart = function() {
 		firstPass = false;
 		start();
 	}
-	var pulse = setInterval( restart, 1000 );
+	pulse = setInterval( restart, interval );
 
 	return folderWatcher;
 };
